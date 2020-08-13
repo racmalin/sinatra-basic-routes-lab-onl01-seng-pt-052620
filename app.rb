d1e6df4aeb994476de731bb6809ffac1865076ad
Kevin C Malin <racmalin@yahoo.com>
2020-08-13 00:58:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+   get '/name' do 
+    "My name is Kevin"
+  end
+
+  get '/hometown' do 
+    "My hometown is GreatFalls"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Wonderful World"
+  end
 end
